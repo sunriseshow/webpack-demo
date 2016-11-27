b11b4195eb8d469c8447a43650699aac4e1d05d9
薛定谔的猫 <zhaoshengdong@meituan.com>
2016-11-28 02:24:51 +0800

    add sourcemap comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 module.exports = {
+    // source-map,具有最好的source map，但是它会减慢打包文件的构建速度；
+    // cheap-module-source-map	, 提高项目构建速度，但只能对应到具体的行，
+    // eval-source-map，不影响构建速度的前提下生成完整的sourcemap，但有性能和安全的隐患，注意不要用于线上！！！
+    // cheap-module-eval-source-map
     devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
+
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/public",//打包后的文件存放的地方
