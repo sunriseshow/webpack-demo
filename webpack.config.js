3cd5cfbf3ad2c04356077964d3dca0d16e5dddb9
薛定谔的猫 <zhaoshengdong@meituan.com>
2016-11-28 03:38:03 +0800

    use .babelrc 分两个配置文件进行配置（webpack会自动调用.babelrc里的babel配置选项）

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
                 test: /\.js$/,
                 exclude: '/node_modules/',
                 loader: 'babel',
-                query: {presets: ['es2015', 'react']},
             },
         ]
     },
