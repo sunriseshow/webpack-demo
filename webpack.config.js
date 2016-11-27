a2ce79d6d1783e6858c064852fa43cf73da51bc8
薛定谔的猫 <zhaoshengdong@meituan.com>
2016-11-28 03:00:09 +0800

    update config: support react

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                 test: /\.js$/,
                 exclude: '/node_modules/',
                 loader: 'babel',
-                query: {presets: ['es2015']},
+                query: {presets: ['es2015', 'react']},
             },
         ]
     },
