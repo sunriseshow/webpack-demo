e4076b592d45f6f0c426b5a95f9984a0aaa8683c
薛定谔的猫 <zhaoshengdong@meituan.com>
2016-11-28 00:12:51 +0800

    add source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/public",//打包后的文件存放的地方
